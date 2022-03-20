069bb212f461093c54e19aa97abf3475577c57f3
pushpit-J19 <54130460+pushpit-J19@users.noreply.github.com>
2022-03-20 20:54:22 +0530

    login register thoda sa

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require("express");
 var bodyParser = require('body-parser');
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
+
 const app = express();
 
+// setting up session middleware
+app.use(sessions({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized:true,
+    cookie: { maxAge: oneDay },
+    resave: false
+}));
+var session;
 
-var mongoClient = require("mongodb").MongoClient;
-// var url = "mongodb://localhost:27017/";
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(__dirname));
+app.use(cookieParser());
 app.use(express.static(__dirname+ '/public'));
+
+// MongoDB setup
+var mongoClient = require("mongodb").MongoClient;
 var url = "mongodb+srv://pushpit:pass@cluster0.m1kld.mongodb.net/wherever_we_go?retryWrites=true&w=majority"
 
 
@@ -23,6 +39,7 @@ app.get("/blogs", (req, res) => {
     })
     
     */
+    session=req.session;
 
     mongoclient.connect(url, function(err, db) {
         if (err) throw err;
@@ -30,7 +47,9 @@ app.get("/blogs", (req, res) => {
         var dbo = db.db("wherever_we_go");
         dbo.collection("blogs").find({}).toArray( (err, result) => {
             if (err) throw err;
-            res.json(result);
+            if(session.userid)
+                res.json({result : result, session : req.session.userid});
+            else res.json({result : result, session : ''});
         })
         db.close();
     });
@@ -48,15 +67,18 @@ app.post("/login", (req,res) => {
     mongoclient.connect(url, function(err, db) {
         if (err) throw err;
         var enteredEmail = req.body.email;
-        var enteredPass = req.body.pass;
+        var enteredPass = req.body.password;
         var dbo = db.db("wherever_we_go");
         dbo.collection("user").find({ email : enteredEmail }).toArray( (err, result) => {
             if (err) throw err;
             
             if (result.length == 0) res.json( {error : "No such user exists"} );
             else if (result[0].password != enteredPass) res.json( {error : "Incorrect password"} );
-            else if (result[0].password == enteredPass) res.redirect("/layout");
-
+            else if (result[0].password == enteredPass) {
+                session=req.session;
+                session.userid=req.body.email;
+                res.redirect("/");
+            }
         })
         db.close();
     });
@@ -76,6 +98,7 @@ app.post ("/register", (req, res) => {
             name : req.body.name,
             email : req.body.email,
             password : req.body.password,
+            state : req.body.state
         };
         
         dbo.collection("users").insertOne(obj, function(err, res) {
@@ -88,4 +111,12 @@ app.post ("/register", (req, res) => {
  
 })
     
+
+// Logout functionality
+app.get('/logout',(req,res) => {
+    req.session.destroy();
+    res.redirect('/');
+});
+
+
 app.listen(8080);
