d15aad21e5f67fe2ab5a546ef8d82f45ffe68613
pushpit-J19 <54130460+pushpit-J19@users.noreply.github.com>
2022-03-21 13:45:53 +0530

    admin

--- app.js
+++ app.js
@@ -31,9 +31,12 @@ app.get("/", (req,res) => {
     res.sendFile(__dirname + "/layout.html");
 });
 
-app.get("/blogs", (req, res) => {
 
+app.get('/index', (req, res) => {
+    var returnObj = {};
     session = req.session;
+    console.log(session.userid);
+    returnObj.sessions = session.userid ? session.userid : null;
 
     mongoclient.connect(url, function(err, db) {
         if (err) throw err;
@@ -41,9 +44,43 @@ app.get("/blogs", (req, res) => {
         var dbo = db.db("wherever_we_go");
         dbo.collection("blogs").find({}).toArray( (err, result) => {
             if (err) throw err;
-            res.json(result);
-        })
-    });
+            returnObj.blogs = result;
+            res.json(returnObj);
+        });    
+    })    
+});
+
+app.get("/requests", (req, res) =>{
+    session = req.session;
+    if (session.userid){
+        if (session.userid[0] == "000"){
+            mongoclient.connect(url, function(err, db) {
+                if (err) throw err;
+        
+                var dbo = db.db("wherever_we_go");
+                dbo.collection("premium_requests").find({}).toArray( (err, result) => {
+                    if (err) throw err;
+                    res.json(result);
+                });
+            }) 
+        }
+        else {
+            mongoclient.connect(url, function(err, db) {
+                if (err) throw err;
+                session = req.session;
+                var dbo = db.db("wherever_we_go");
+                dbo.collection("premium_requests").find({_id : session.userid[0]}).toArray( (err, result) => {
+                    if (err) throw err;
+                    if (result.length == 0){
+                        res.json("0");
+                    }else{
+                        res.json("exists");
+                    }
+                });
+            })
+        }
+    }else res.json("0");
+    
 })
 
 
@@ -61,14 +98,15 @@ app.post("/login", (req,res) => {
         var enteredPass = req.body.password;
         var dbo = db.db("wherever_we_go");
         dbo.collection("users").find({ _id : enteredEmail }).toArray( (err, result) => {
-            console.log(result);
+           // console.log(result);
             if (err) throw err;
             
             if (result.length == 0) res.json( "No such user exists");
             else if (result[0].password != enteredPass) res.json( "Incorrect password" );
             else if (result[0].password == enteredPass) {
                 session=req.session;
-                session.userid=req.body.email;
+                session.userid=[req.body.email, result[0].premium] ;
+             //   console.log(session.userid);
             }
             db.close();
             res.json("success");
@@ -111,17 +149,41 @@ app.get('/logout',(req,res) => {
     res.redirect('/');
 });
 
-app.get('/checkSession', (req, res) => {
-    session = req.session;
-    console.log(session.userid);
-    res.send(session.userid)
-});
-
 app.get("/blog/:id", (req, res) => {
     res.send("hello");
 });
 
+
+// Premium 
+
+app.get("/prem_apply", (req, res) => {
+
+    mongoclient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("wherever_we_go");
+        session = req.session;
+        console.log(session.userid);
+        var obj = {
+            userid : req.session.userid[0],
+            status : false,
+        };
+                
+        dbo.collection("premium_requests").insertOne(obj, function(err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close();
+        });
+    });
+
+    res.redirect("/");
+})
+
+
+
+
 // express mongodb cookie-parser express-session multer fs path 
+
+// Insert blog
 
 app.get("/add_blog", (req,res) => {
     res.sendFile(__dirname + "/add_blog.html");
